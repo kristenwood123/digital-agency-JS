45ca259df24a3603e37d249652306d81dffc06b3
Kristen <kristenwood123@gmail.com>
2021-03-09 17:07:29 -0800

    added navbar

---
+++ app.js
@@ -0,0 +1,8 @@
+// toggle Navbar
+
+let mainNav = document.getElementById('js-menu')
+let navBarToggle = document.getElementById('js-navbar-toggle')
+
+navBarToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('active')
+})
